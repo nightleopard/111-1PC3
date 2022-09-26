d6990817eb91ef627f11ad8fb6dc445a1fd5087a
nightleopard <win20011018@gmail.com>
2022-09-26 14:02:28 +0800

    pc3

---
+++ 111-1PC3/Test.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1PC3
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int i_Clothes = 300;
+            int i_Hat = 350;
+            int i_Pants = 400;
+            int sum = i_Clothes + i_Hat + i_Pants;
+            Response.Write("sum");
+        }
+    }
+}
